df767525cf616ea93ba0bb8d8cfae1f706822266
Dmitry Voitekh <d.voitekh@mail.ru>
2016-03-24 07:55:17 -0700

    Rails Blog

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,20 @@
+class CommentsController < ApplicationController
+  def comment_params
+    params.require(:comment).permit(:text, :post_id)
+  end
+
+  def create
+  	@post = Post.find(params[:post_id])
+    @comment = @post.comments.build(comment_params) 
+    @comment.save
+    redirect_to @post
+  end
+
+  def destroy
+    @post = Post.find params[:post_id]
+    @comment = @post.comments.find params[:id]
+    @comment.destroy
+    redirect_to @post
+  end
+
+end
